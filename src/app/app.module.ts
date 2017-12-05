5ec23d35c4b5a809f80722d1f1536c1d827d152b
Paulius Jazauskas <pjaz@danskebank.lt>
2017-12-05 13:41:29 +0100

    added fundamental components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { MenuComponent } from './menu/menu.component';
+import { ItemDashboardComponent } from './item-dashboard/item-dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+const appRoutes: Routes =
+[
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'shop/:type',
+    component: ShopComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    MenuComponent,
+    ItemDashboardComponent,
+    HomeComponent,
+    ShopComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
