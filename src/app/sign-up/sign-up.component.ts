4074c324e950b447ce5a21a119afb36a180e4b76
silent-hero <p.jazauskas@gmail.com>
2017-12-17 18:39:38 +0200

    finished sign-up form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -12,6 +12,10 @@ export class SignUpComponent implements OnInit {
   form: FormGroup;
 
   name: FormControl;
+  surname: FormControl;
+  email: FormControl;
+  password1: FormControl;
+  password2: FormControl;
 
   constructor(public validatorService: ValidatorService) { }
 
@@ -24,13 +28,21 @@ export class SignUpComponent implements OnInit {
   public CreateFormControls() 
   {
     this.name = new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9_-]*"), Validators.minLength(2), Validators.maxLength(20)]);
+    this.surname = new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9_-]*"), Validators.minLength(2), Validators.maxLength(50)]);
+    this.email = new FormControl('', [Validators.required, Validators.pattern("[^ @]*@[^ @]*"), Validators.minLength(2), Validators.maxLength(50)]);
+    this.password1 = new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9_-]*"), Validators.minLength(4), Validators.maxLength(50)]);
+    this.password2 = new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9_-]*"), Validators.minLength(4), Validators.maxLength(50)]);
   }
 
   public CreateForm()
   {
     this.form = new FormGroup
     ({
-      name: this.name
+      name: this.name,
+      surname: this.surname,
+      email: this.email,
+      password1: this.password1,
+      password2: this.password2
     })
   }
 
