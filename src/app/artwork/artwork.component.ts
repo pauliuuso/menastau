824f3411a9e1cae307a442d923dab3ff8861a68d
Paulius Jazauskas <pjaz@danskebank.lt>
2018-01-19 14:36:13 +0100

    changed gallery to have a links, added method to get work info in full view

--- src/app/artwork/artwork.component.ts
+++ src/app/artwork/artwork.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SharedService, IWork } from '../shared.service';
+import { UserService } from '../user.service';
+import { Subject } from "rxjs/Subject";
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-artwork',
   templateUrl: './artwork.component.html',
   styleUrls: ['./artwork.component.css']
 })
-export class ArtworkComponent implements OnInit {
+export class ArtworkComponent implements OnInit, OnDestroy
+{
+  unsubcribe: Subject<void> = new Subject<void>();
+  workId: string;
+  work: IWork;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private sharedService: SharedService, private userService: UserService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
+    this.activatedRoute.params.subscribe(params =>
+    {
+      this.workId = params["id"];
+      this.GetWorkInfo(params["id"]);
+    });
+  }
+
+  ngOnDestroy()
+  {
+    this.unsubcribe.next();
+    this.unsubcribe.complete();
+  }
+
+  public GetWorkInfo(id: string)
+  {
+    this.sharedService.GetOneWork(id).takeUntil(this.unsubcribe).subscribe
+    (
+      data =>
+      {
+        this.work = data;
+      },
+      error =>
+      {
+        this.errorMessage = error.message;
+      }
+    );
   }
 
 }
